a61e34c9a0dc1c9ac5f972ce5b9d367ad37940e3
andreicioc133 <andrei.cioc97@gmail.com>
2025-02-25 14:05:04 +0200

    added objects for locations

--- script.js
+++ script.js
@@ -23,6 +23,16 @@ const locations = [
     "button functions": [goStore, goCave, fightDragon],
     text: 'You are in the town square. You see a sign that says "Store".',
   },
+  {
+    name: "store",
+    "button text": [
+      "Buy 10 health (10 gold)",
+      "Buy weapon (30 gold)",
+      "Go to town square",
+    ],
+    "button functions": [buyHealth, buyWeapon, goTown],
+    text: "You enter the store.",
+  },
 ];
 
 // initialize buttons
@@ -30,9 +40,7 @@ button1.onclick = goStore;
 button2.onclick = goCave;
 button3.onclick = fightDragon;
 
-function update(location) {}
-
-function goTown() {
+function update(location) {
   button1.innerText = "Go to store";
   button2.innerText = "Go to cave";
   button3.innerText = "Fight dragon";
@@ -43,15 +51,9 @@ function goTown() {
     'You are in the town square. You see a sign that says "Store".';
 }
 
-function goStore() {
-  button1.innerText = "Buy 10 health (10 gold)";
-  button2.innerText = "Buy weapon (30 gold)";
-  button3.innerText = "Go to town square";
-  button1.onclick = buyHealth;
-  button2.onclick = buyWeapon;
-  button3.onclick = goTown;
-  text.innerText = "You enter the store.";
-}
+function goTown() {}
+
+function goStore() {}
 
 function goCave() {
   console.log("Going to cave.");
